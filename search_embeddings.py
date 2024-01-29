80d2eb101db420088718bc580b52d4ef2c68825c
andreaskundig <andreas.kuendig@gmx.ch>
2024-01-29 22:06:40 +0100

    print_verse

--- search_embeddings.py
+++ search_embeddings.py
@@ -4,12 +4,14 @@ import numpy as np
 from book import get_lines, BOOKS
 from pathlib import Path
 from make_embeddings import get_model, save_embeddings_to_json
-from typing import List, TypedDict
+from typing import List, TypedDict, Dict
 
 class EmbeddingData(TypedDict):
     ids: List[int]
     embeddings: List[List[float]]
     faiss_index: faiss.IndexFlatL2
+    title: str
+    verses: Dict[str, str]
 
 book_path = Path("split/out")
 
@@ -43,7 +45,11 @@ def get_or_create_embeddings(book_index) -> EmbeddingData:
     with open(embeddings_path) as file:
         data = json.load(file)
         data['faiss_index'] = build_faiss_index(data['embeddings'])
-        return data
+
+    book_title = BOOKS[book_index]
+    data['title'] = book_title
+    data['verses'] = dict(get_lines(book_path / book_title))
+    return data
 
 def find_next_verse_id(verse1_id, data1, data2):
     embedding = verse_id_to_embedding(data1, verse1_id)
@@ -54,18 +60,20 @@ def find_next_verse_id(verse1_id, data1, data2):
         return None
     return verse2_ids[verse2_index+1]
 
+def print_verse(data, verse_id):
+    verse = data['verses'][verse_id]
+    print(f'{verse_id} {verse}')
+
 #TODO alternate verses from two books
 def book_dialog(book_idx_1, book_idx_2):
     book_indices = [book_idx_1, book_idx_2]
     data_list = [get_or_create_embeddings(idx) for idx in book_indices]
-    book_titles = [BOOKS[book_idx_1], BOOKS[book_idx_2]]
-    texts = [dict(get_lines(book_path / bt)) for bt in book_titles]
+
     verse1_id = "1:1"
-    verse1 = texts[0][verse1_id]
-    print(f'{verse1_id} {verse1}')
+    print_verse(data_list[0], verse1_id)
+
     verse2_id = find_next_verse_id(verse1_id, *data_list)
-    verse2 = texts[1][verse2_id]
-    print(f'{verse2_id} {verse2}')
+    print_verse(data_list[1], verse2_id)
 
 # choosing first and last index
 # genesis vs apocalypse
